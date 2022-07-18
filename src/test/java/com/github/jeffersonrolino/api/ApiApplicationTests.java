062ea0ccaa1ab5182d10fb809a62bf42e2c94c00
JeffersonRolino <jefferson.rolino@gmail.com>
2022-07-18 19:59:24 -0300

    Boilerplate do projeto

---
+++ src/test/java/com/github/jeffersonrolino/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jeffersonrolino.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
